e0161d61207af384fecbf0123f67d790586c55b8
Josh Merlino <joshua.merlino@gmail.com>
2020-08-28 15:13:21 -0400

    add readme among other things

---
+++ README.md
@@ -0,0 +1,1 @@
+Invite [Josh O' Bot](https://discord.com/api/oauth2/authorize?client_id=748971236276699247&permissions=0&scope=bot) to your Discord Server!
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const Discord = require("discord.js");
+
+// Initialize client instance
+const client = new Discord.Client();
+
+// Log in with the token in the enviroment file
+client.login(process.env.CLIENT_SECRET);
+
+// When the client logs in, initialize the client runtime.
+client.on("ready", () => require("./src/ClientRuntime.js")(client));
