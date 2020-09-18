39b5bc2b629c5df9c56ebaad3983e86f97ec30f1
Josh Merlino <joshua.merlino@gmail.com>
2020-09-17 23:12:27 -0400

    update language

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -39,7 +39,7 @@ module.exports = class Command extends require("../Command.js") {
 			const embed = new MessageEmbed()
 			.setColor(guildConfig.theme.primary)
 			guildConfig.commands["image"].enabled && embed.addField("Image Search", `\`${guildConfig.prefix}${guildConfig.commands["image"].alias[0]} <term>\``)
-			guildConfig.commands["urban"].enabled && embed.addField("Urban", `\`${guildConfig.prefix}${guildConfig.commands["urban"].alias[0]} <term>\``)
+			guildConfig.commands["urban"].enabled && embed.addField("Urban Dictionary", `\`${guildConfig.prefix}${guildConfig.commands["urban"].alias[0]} <term>\``)
 			return channel.send(embed);
 		}
 
