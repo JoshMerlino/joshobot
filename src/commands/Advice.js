d12f38e5add3dd22104c554140103491532a6b94
Josh Merlino <joshua.merlino@gmail.com>
2020-09-20 13:46:54 -0400

    add ?advice

---
+++ src/commands/Advice.js
@@ -0,0 +1,22 @@
+module.exports = class Command extends require("../Command.js") {
+
+	constructor() {
+		super("advice", ...arguments);
+	}
+
+	async onCommand({ args, sender, guildConfig, root, channel, guild }) {
+
+		const user = guild.member(args[0] ? args[0].replace(/[\\<>@#&!]/g, "") : sender.id);
+		const { slip } =await fetch(`https://api.adviceslip.com/advice`).then(resp => resp.json());
+
+		const embed = new MessageEmbed();
+		embed.setColor(guildConfig.theme.info);
+		embed.setTitle(`A word from the wise`);
+		embed.setDescription(slip.advice);
+		embed.setFooter(sender.displayName, sender.user.displayAvatarURL());
+
+		await channel.send(embed);
+
+	}
+
+}
--- src/commands/Help.js
+++ src/commands/Help.js
@@ -40,6 +40,7 @@ module.exports = class Command extends require("../Command.js") {
 			const embed = new MessageEmbed()
 			.setColor(guildConfig.theme.primary)
 			.setFooter(sender.displayName, sender.user.displayAvatarURL())
+			guildConfig.commands["Advice"].enabled && embed.addField("Advice", `\`${guildConfig.prefix}${guildConfig.commands["avatar"].alias[0]}\``)
 			guildConfig.commands["avatar"].enabled && embed.addField("Avatar", `\`${guildConfig.prefix}${guildConfig.commands["avatar"].alias[0]} (@user = you)\``)
 			guildConfig.commands["color"].enabled && embed.addField("Color", `\`${guildConfig.prefix}${guildConfig.commands["color"].alias[0]} [hex code | @user]\``)
 			guildConfig.commands["image"].enabled && embed.addField("Image Search", `\`${guildConfig.prefix}${guildConfig.commands["image"].alias[0]} <term>\``)
