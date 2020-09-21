5559f73c8c870e6a1b3510a61260925d17be0f96
Josh Merlino <joshua.merlino@gmail.com>
2020-09-21 12:25:04 -0400

    add presence status

--- src/ClientRuntime.js
+++ src/ClientRuntime.js
@@ -64,4 +64,13 @@ module.exports = async function() {
 		if(process.env.MODE === "PRODUCTION") await exec("sudo service node@josh-o-bot restart");
 	});
 
+	client.user.setPresence({
+	    activity: {
+	        name: `?help | ${Object.keys(global.config).length + 1} Servers`,
+	        application: "github.com",
+			type: "PLAYING",
+	    },
+	    status: "online"
+	})
+
 };
