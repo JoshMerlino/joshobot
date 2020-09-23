b2ae6cea66d6d64d4855c85bced37d0ed1fe0f02
Josh Merlino <joshua.merlino@gmail.com>
2020-09-23 15:48:32 -0400

    finally fix

--- src/ClientRuntime.js
+++ src/ClientRuntime.js
@@ -50,7 +50,7 @@ module.exports = async function() {
 			})
 
 			// Refresh configuration for this specific guild
-			global.config[guild.id] = YAML.parse(await fs.readFile(path.join(APP_ROOT ,"config", `guild_${guild.id}.yml`)));
+			global.config[guild.id] = YAML.parse(await fs.readFile(path.join(APP_ROOT ,"config", `guild_${guild.id}.yml`), "utf8"));
 
 		}, 1000);
 
