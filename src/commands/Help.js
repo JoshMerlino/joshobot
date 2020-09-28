454a7c1540f106ad2eac9024d43284bb2df1b4be
Josh Merlino <joshua.merlino@gmail.com>
2020-09-28 12:21:32 -0400

    add namemc lookup

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -47,6 +47,7 @@ module.exports = class Command extends require("../Command.js") {
 			guildConfig.commands["avatar"].enabled && embed.addField("Avatar", `\`${guildConfig.prefix}${guildConfig.commands["avatar"].alias[0]} (@user = you)\``)
 			guildConfig.commands["color"].enabled && embed.addField("Color", `\`${guildConfig.prefix}${guildConfig.commands["color"].alias[0]} [hex code | @user]\``)
 			guildConfig.commands["image"].enabled && embed.addField("Image Search", `\`${guildConfig.prefix}${guildConfig.commands["image"].alias[0]} <term>\``)
+			guildConfig.commands["namemc"].enabled && embed.addField("Name MC Lookup", `\`${guildConfig.prefix}${guildConfig.commands["namemc"].alias[0]} <minecraft username>\``)
 			guildConfig.commands["poll"].enabled && embed.addField("Create Poll", `\`${guildConfig.prefix}${guildConfig.commands["poll"].alias[0]} <message>\``)
 			guildConfig.commands["reddit"].enabled && embed.addField("Reddit Search", `\`${guildConfig.prefix}${guildConfig.commands["reddit"].alias[0]} <term>\``)
 			guildConfig.commands["urban"].enabled && embed.addField("Urban Dictionary", `\`${guildConfig.prefix}${guildConfig.commands["urban"].alias[0]} <term>\``)
---
+++ src/commands/Namemc.js
@@ -0,0 +1,37 @@
+const { lookupName } = require("namemc")
+
+module.exports = class Command extends require("../Command.js") {
+
+	constructor() {
+		super("namemc", ...arguments);
+	}
+
+	async onCommand({ args, sender, guildConfig, root, channel, guild, audit }) {
+
+		const [ mcname ] = args;
+
+
+		const embed = new MessageEmbed();
+		embed.setColor(guildConfig.theme.info);
+		embed.setFooter(sender.displayName, sender.user.displayAvatarURL());
+
+		try {
+			const [ mcuser ] = await lookupName(mcname);
+			embed.setURL(`https://mine.ly/${mcuser.profileId}`);
+			embed.setTitle(mcuser.currentName);
+			embed.addField("UUID", `\`${mcuser.uuid}\``);
+			embed.addField("Previous Names", mcuser.pastNames.map(({ name }) => `\`${name}\``), true);
+			embed.addField("Changed At", mcuser.pastNames.map(({ changedAt }) => `${changedAt !== null ? "`" + dayjs(changedAt).fromNow() + "`" : ""}`), true);
+			embed.setThumbnail(mcuser.imageUrls.body);
+			return channel.send(embed)
+		} catch(e) {
+			return channel.send(new MessageEmbed()
+			.setColor(guildConfig.theme.error)
+			.setDescription(`Minecraft user "${mcname}" was not found!`)
+			.setFooter(sender.displayName, sender.user.displayAvatarURL()))
+		}
+
+
+	}
+
+}
