432b082ec3cafe7aa499b1162d441ce9a389875e
Josh Merlino <joshua.merlino@gmail.com>
2020-10-04 13:24:42 -0400

    move ?whois to moderator commands

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -36,6 +36,7 @@ module.exports = class Command extends require("../Command.js") {
 			guildConfig.commands["purge"].enabled && embed.addField("Purge Messages", `\`${guildConfig.prefix}${guildConfig.commands["purge"].alias[0]} <length>\``)
 			guildConfig.commands["role"].enabled && embed.addField("Manage Roles", `\`${guildConfig.prefix}${guildConfig.commands["role"].alias[0]} <add|remove> <role> <@user>\``)
 			guildConfig.commands["unmute"].enabled && embed.addField("Unmute Members", `\`${guildConfig.prefix}${guildConfig.commands["unmute"].alias[0]} <@user>\``)
+			guildConfig.commands["user"].enabled && embed.addField("User Information", `\`${guildConfig.prefix}${guildConfig.commands["user"].alias[0]} [@user = you]\``)
 			guildConfig.commands["warn"].enabled && embed.addField("Warn Members", `\`${guildConfig.prefix}${guildConfig.commands["warn"].alias[0]} <@user> [reason]\``)
 			return channel.send(embed);
 		}
@@ -54,7 +55,6 @@ module.exports = class Command extends require("../Command.js") {
 			guildConfig.commands["poll"].enabled && embed.addField("Create Poll", `\`${guildConfig.prefix}${guildConfig.commands["poll"].alias[0]} <message>\``)
 			guildConfig.commands["reddit"].enabled && embed.addField("Reddit Search", `\`${guildConfig.prefix}${guildConfig.commands["reddit"].alias[0]} <term>\``)
 			guildConfig.commands["urban"].enabled && embed.addField("Urban Dictionary", `\`${guildConfig.prefix}${guildConfig.commands["urban"].alias[0]} <term>\``)
-			guildConfig.commands["user"].enabled && embed.addField("User Information", `\`${guildConfig.prefix}${guildConfig.commands["user"].alias[0]} [@user = you]\``)
 			return channel.send(embed);
 		}
 
