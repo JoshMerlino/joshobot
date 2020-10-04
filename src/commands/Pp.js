8e9a502e766c5e3eec7d4a857c50c550f84dc8cf
Josh Merlino <joshua.merlino@gmail.com>
2020-10-04 12:31:05 -0400

    update ?pp size constant

--- src/commands/Pp.js
+++ src/commands/Pp.js
@@ -11,12 +11,12 @@ module.exports = class Command extends require("../Command.js") {
 		const user = guild.member(args.length > 0 ? args[0].replace(/[\\<>@#&!]/g, "") : sender.id);
 
 		const id = parseInt(user.user.id.substr(2, 6));
-		const size = id * .000018;
+		const size = id * .000009;
 
 		const embed = new MessageEmbed();
 		embed.setColor(guildConfig.theme.info);
 		embed.setFooter(sender.displayName, sender.user.displayAvatarURL());
-		embed.setTitle(`${user.displayName}'s PP's Size`);
+		embed.setTitle(`${user.displayName}'s PP Size`);
 		embed.setDescription(`Estimated Size: **${Math.floor(size*10)/10}in**\n${ppbottom}${ new Array(Math.floor(size - 2)).fill(ppmiddle).join("") }${pphead}`);
 		channel.send(embed);
 
