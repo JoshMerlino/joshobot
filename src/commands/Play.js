d58c6f39979851db6139129d20e0d6e6f49c5d8f
Josh Merlino <joshua.merlino@gmail.com>
2020-10-04 21:17:47 -0400

    deafen bot

--- src/commands/Play.js
+++ src/commands/Play.js
@@ -69,7 +69,7 @@ module.exports = class Command extends require("../Command.js") {
 		} catch(e) {
 			return channel.send(new MessageEmbed()
 			.setColor(guildConfig.theme.error)
-			.setDescription(`Uh oh! This search yielded age-restricted content.`)
+			.setDescription(`Uh oh! This search yielded unavailable or age-restricted content.`)
 			.setFooter(sender.displayName, sender.user.displayAvatarURL()));
 		}
 
@@ -86,6 +86,7 @@ module.exports = class Command extends require("../Command.js") {
 		.setFooter(sender.displayName, sender.user.displayAvatarURL()));
 
 		const connection = await sender.voice.channel.join();
+		connection.voice.setSelfDeaf(true);
 		const stream = ytdl(song.url, { filter: "audioonly" });
 		const dispatcher = connection.play(stream);
 
