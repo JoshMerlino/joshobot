6059f276fc976576d55b022ace635165adbecee7
Josh Merlino <joshua.merlino@gmail.com>
2020-10-11 19:37:06 -0400

    update ?mc

--- src/commands/Namemc.js
+++ src/commands/Namemc.js
@@ -10,18 +10,26 @@ module.exports = class Command extends require("../Command.js") {
 		}]);
 	}
 
-	async onCommand({ args, sender, guildConfig, root, channel, guild, audit }) {
+	async onCommand({ args, sender, guildConfig, channel }) {
 
 		const [ mcname ] = args;
 
 		const embed = new MessageEmbed();
-		embed.setColor(guildConfig.theme.info);
+		embed.setTitle("Minecraft Account Lookup")
 		embed.setFooter(sender.displayName, sender.user.displayAvatarURL());
+
+		if(args.length !== 1) {
+			embed.setColor(guildConfig.theme.warn);
+			embed.addField("Description", this.description, true)
+			embed.addField("Usage", this.usage, true)
+            return await channel.send(embed);
+		}
 
 		try {
 			const [ mcuser ] = await lookupName(mcname);
 			embed.setURL(`https://mine.ly/${mcuser.profileId}`);
 			embed.setTitle(mcuser.currentName);
+			embed.setColor(guildConfig.theme.info);
 			embed.addField("UUID", `\`${mcuser.uuid}\``);
 			embed.addField("Previous Names", mcuser.pastNames.map(({ name }) => `\`${name}\``), true);
 			embed.addField("Changed At", mcuser.pastNames.map(({ changedAt }) => `${changedAt !== null ? "`" + dayjs(changedAt).fromNow() + "`" : "__Never__"}`), true);
