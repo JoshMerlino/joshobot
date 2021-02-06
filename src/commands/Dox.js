46bd2c8d308de8780e356e49b9d4f7b3434949d2
Josh Merlino <joshua.merlino@gmail.com>
2021-02-05 22:56:16 -0500

    add ?dox

---
+++ src/commands/Dox.js
@@ -0,0 +1,45 @@
+module.exports = class Command extends require("../Command.js") {
+
+	constructor() {
+		super("dox", ...arguments);
+		this.register("Look up an IP Address. 📍", HelpSection.MISCELLANEOUS, [{
+			argument: "IP Address",
+			required: true,
+		}]);
+	}
+
+	async onCommand({ args, sender, channel, guildConfig }) {
+
+		// Parse arguments
+		const [ query ] = args;
+
+		// Look up ip address
+		const details = await fetch(`https://joshm.us.to/api/iputils/v1/lookup?query=${query}`).then(resp => resp.json());
+
+		console.log(details)
+
+		// Formulate embed
+		const embed = new MessageEmbed();
+		embed.setTitle(`Geolocation for: ${query}`);
+		embed.setFooter(sender.displayName, sender.user.displayAvatarURL());
+
+		// If bad request
+		if(details.success === false) {
+			embed.setColor(guildConfig.theme.error);
+			embed.setDescription(details.error);
+			return await channel.send(embed);
+		}
+
+		// If successful
+		embed.setColor(guildConfig.theme.info);
+		embed.setURL(details.mapUrl);
+		embed.addField("Location", `${details.city} ${details.region}, ${details.zip}, ${details.country}`);
+		embed.addField("Lat & Lon", `${details.lat}, ${details.lon}`, true);
+		embed.addField("Timezone", details.timezone, true);
+		embed.addField("Internet Service Provider", details.org, true);
+
+		return await channel.send(embed);
+
+	}
+
+}
