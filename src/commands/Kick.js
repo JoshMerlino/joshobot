c9101486d029fc4aa13ee2d0d7e8de41e605d16e
Josh Merlino <joshua.merlino@gmail.com>
2021-02-11 18:34:14 -0500

    update kick

--- src/commands/Kick.js
+++ src/commands/Kick.js
@@ -21,23 +21,18 @@ module.exports = class Command extends require("../Command.js") {
 	async onCommand({ args, sender, guildConfig, channel, guild }) {
 
 		// Make sure sender is a bot master
-		if(!util.hasPermissions(sender, guildConfig, "KICK_MEMBERS")) return await util.noPermissions(channel, sender);
+		if(!util.hasPermissions(sender, guildConfig, "KICK_MEMBERS")) return;
+
+		// If not enough args
+		if(args.length < 1) return this.sendUsage(channel);
 
 		// Get arguments
-		const [ user = null, ...reason ] = args;
+		const [ user, ...reason ] = args;
 
 		// Start formulating embed
 		const embed = new MessageEmbed();
 		embed.setTitle("Kick")
 		embed.setFooter(sender.user.tag, sender.user.displayAvatarURL());
-
-		// If not enough params
-		if(user === null) {
-			embed.setColor(Color.warn);
-			embed.addField("Description", this.description, true)
-			embed.addField("Usage", this.usage, true)
-            return await channel.send(embed);
-		}
 
 		// get guild member
 		const member = util.user(user, guild);
@@ -45,20 +40,18 @@ module.exports = class Command extends require("../Command.js") {
 		// If ban target has ban permissions
 		if(util.hasPermissions(member, guildConfig, "KICK_MEMBERS")) {
 			embed.setColor(Color.server);
-			embed.addField("Error", `${member.toString()} is not able to be kicked. They most likley have equal or greater permissions than you.`, true)
+			embed.setDescription(`${member.toString()} is not able to be kicked.\nThey most likley have equal or greater permissions than you.`);
             return await channel.send(embed);
 		}
 
-		member.kick(reason.join(" ")).then(async function() {
+		// Ban member
+		member.ban(reason.join(" ")).then(async function() {
 			embed.setColor(Color.success);
-			embed.addField("User", member.toString(), true);
-			reason.length > 0 && embed.addField("Reason", reason.join(" "), true);
-			return await channel.send(embed);
+			embed.setDescription(`Kicked ${member.toString()}${reason.length > 0 ? " for ":""}${reason.join(" ")}`);
 		}).catch(async error => {
 			embed.setColor(Color.error);
-			embed.addField("Error", error.toString().split(":")[2], true)
-            return await channel.send(embed);
-		})
+			embed.setDescription(`${member.toString()} is not able to be kicked.\n${error.toString().split(":")[2]}`);
+		}).finally(async () =>  await channel.send(embed));
 
 	}
 
