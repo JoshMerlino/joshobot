765fc76a655fa058ab19eab882cf5a5155c1306d
Josh Merlino <joshua.merlino@gmail.com>
2021-02-10 22:27:39 -0500

    add botmaster

--- src/commands/Botmaster.js
+++ src/commands/Botmaster.js
@@ -9,6 +9,9 @@ module.exports = class Command extends require("../Command.js") {
 			"Manages who has permission overrides for the bot. 🤖",
 			HelpSection.MODERATION,
 			[{
+				argument: "add | remove",
+				required: true,
+			}, {
 				argument: "@User | @Role",
 				required: true,
 			}]
@@ -18,39 +21,71 @@ module.exports = class Command extends require("../Command.js") {
 	async onCommand({ args, sender, guildConfig, channel, guild }) {
 
 		// Make sure sender is a bot master
-		if(!util.hasPermissions(sender, guildConfig, "MANAGE_ROLES")) return await util.noPermissions(channel, sender)
+		if(!util.hasPermissions(sender, guildConfig, "MANAGE_ROLES")) return;
+
+		// If not enough params
+		if(args.length < 2) return this.sendUsage(channel);
 
 		// Get params
-		const [ subcommand = null, userOrRole = "" ] = args;
+		const [ subcommand, userOrRole ] = args;
 		const id = userOrRole.replace(/[\\<>@#&!]/g, "");
 
 		// Start formulating embed
 		const embed = new MessageEmbed();
 		embed.setTitle("Bot Masters");
-		embed.setFooter(sender.displayName, sender.user.displayAvatarURL());
+		embed.setFooter(sender.user.tag, sender.user.displayAvatarURL());
 
-		if(userOrRole === "" || subcommand === null) {
-			embed.setColor(Color.warn);
-			embed.addField("Description", this.description, true)
-			embed.addField("Usage", this.usage, true)
-            return await channel.send(embed);
-		}
+		// Switch subcommand
+		switch(subcommand.toLowerCase()) {
 
-		if(subcommand.toLowerCase() === "add") {
-			config[guild.id].botmasters.push(id);
-			embed.setColor(Color.success);
-			embed.setDescription(`Added ${userOrRole} to bot masters.`);
-			await util.writeConfig(guild.id);
-            return await channel.send(embed);
-		}
+			// Add
+			case "add":
+			case "a":
 
-		if (subcommand.toLowerCase() === "remove") {
-			config[guild.id].botmasters.splice(config[guild.id].botmasters.indexOf(id));
-			embed.setColor(Color.success);
-			embed.setDescription(`Removed ${userOrRole} from bot masters.`);
-			await util.writeConfig(guild.id);
-            return await channel.send(embed);
-		}
+				// See if user is on botmaster list
+				if(config[guild.id].botmasters.includes(id)) {
+					embed.setColor(Color.error);
+					embed.setDescription(`${userOrRole} is already a bot master.`)
+					return await channel.send(embed);
+				}
+
+				// Add to botmaster
+				config[guild.id].botmasters.push(id);
+
+				// Save config to disk
+				await util.writeConfig(guild.id);
+
+				// Send embed
+				embed.setColor(Color.success);
+				embed.setDescription(`Added ${userOrRole} to bot masters.`);
+				return await channel.send(embed);
+
+			// Remove
+			case "remove":
+			case "r":
+
+				// See if user is on botmaster list
+				if(!config[guild.id].botmasters.includes(id)) {
+					embed.setColor(Color.error);
+					embed.setDescription(`${userOrRole} is not a bot master.`)
+					return await channel.send(embed);
+				}
+
+				// Remove from botmaster
+				config[guild.id].botmasters.splice(config[guild.id].botmasters.indexOf(id));
+
+				// Save config to disk
+				await util.writeConfig(guild.id);
+
+				// Send embed
+				embed.setColor(Color.success);
+				embed.setDescription(`Added ${userOrRole} to bot masters.`);
+				return await channel.send(embed);
+
+		};
+
+		// If unknown subcommand
+		return this.sendUsage(channel);
 
 	}
 
