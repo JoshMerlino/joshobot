4b5288e2290e6c29a032b8f21c27829b13004677
Josh Merlino <joshua.merlino@gmail.com>
2021-02-14 21:00:40 -0500

    add bump reminder

--- src/AsyncActions.js
+++ src/AsyncActions.js
@@ -1,28 +1,52 @@
 module.exports = async function(guild) {
 
-	// Schedule repeating tasks
+	// Schedule unmute
 	client.setInterval(async function() {
-
-		// Unmute members when their time expires
 		global.config[guild.id].commands["mute"].persistance.map(async (entry, key) => {
 			if(entry.expires < Date.now()) {
 				global.config[guild.id].commands["mute"].persistance.splice(key, 1);
 				await fs.writeFile(path.join(APP_ROOT ,"config", `guild_${guild.id}.yml`), YAML.stringify(global.config[guild.id]), "utf8");
-				unmute(entry);
+				const { moderator, specimen, channel } = entry;
+				const mod = util.parseCollection(guild.members.cache)[moderator].user
+				guild.member(specimen).roles.remove(global.config[guild.id].commands["mute"].muterole);
+				guild.channels.resolve(channel).send(new MessageEmbed()
+				.setColor(global.config[guild.id].theme.success)
+				.setFooter(guild.member(mod).displayName, guild.member(mod).user.displayAvatarURL())
+				.setDescription(`<@!${specimen}> is no longer muted.`));
 			}
 		})
-
 	}, 1000);
 
-	// Unmute function
-	function unmute({ moderator, specimen, channel }) {
 
-		const mod = util.parseCollection(guild.members.cache)[moderator].user
-		guild.member(specimen).roles.remove(global.config[guild.id].commands["mute"].muterole);
-		guild.channels.resolve(channel).send(new MessageEmbed()
-		.setColor(global.config[guild.id].theme.success)
-		.setFooter(guild.member(mod).displayName, guild.member(mod).user.displayAvatarURL())
-		.setDescription(`<@!${specimen}> is no longer muted.`));
+	// If disboard is in the server
+	if(util.parseCollection(guild.members.cache).hasOwnProperty("709480797781885000")) {
+
+		// Schedule bump reminders
+		client.setInterval(async function() {
+
+			// Get last message channel
+			const { lastMessageChannelID } = await guild.member("709480797781885000");
+
+			// Make sure there is a last message
+			if(lastMessageChannelID === undefined) return;
+
+			// Get channel
+			const channel = await guild.channels.resolve(lastMessageChannelID);
+
+			let messages = Object.values(parseCollection(await channel.messages.fetch({ limit: 25 })));
+			let lastMessage = messages[0];
+			messages = messages.filter(message => message.author.id === "302050872383242240");
+			messages = messages.filter(message => message.embeds[0].color === 2406327);
+			messages = messages.sort((a, b) => b.createdTimestamp - a.createdTimestamp)[0];
+
+			// If a bumps ISNT available cancel action
+			if(lastMessage.author.id === "709480797781885000") return;
+			if(messages !== undefined && Date.now() < messages.createdTimestamp + 2*60*60*1000) return;
+
+			// Send bump message
+			await channel.send(`A bump is now available for this server. Do \`!d bump\``);
+
+		}, 5000);
 
 	}
 
