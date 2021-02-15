9090defaa8355203f10c7f75453aac945a8e8ccb
Josh Merlino <joshua.merlino@gmail.com>
2021-02-15 12:39:28 -0500

    update image to use own api

--- src/commands/Image.js
+++ src/commands/Image.js
@@ -25,24 +25,39 @@ module.exports = class Command extends require("../Command.js") {
 		channel.startTyping();
 
 		// Get images from bing
-		const { value } = await fetch(`https://bing-image-search1.p.rapidapi.com/images/search?q=${encodeURIComponent(term)}`, {
-		    headers: {
-		        "x-rapidapi-key": process.env.RAPID_API_KEY,
-		        useQueryString: true
-		    }
-		}).then(resp => resp.json())
-		  .finally(() => channel.stopTyping())
+		const { images } = await fetch(`https://joshm.us.to/api/v1/image?q=${encodeURIComponent(term)}`)
+		  .then(resp => resp.json())
+		  .finally(() => channel.stopTyping());
 
-		// Pick a random image from search
-		const image = value[Math.floor(Math.random() * value.length)];
 
 		// Formulate embed
 		const embed = new MessageEmbed();
 		embed.setTitle(`Image Search for "${term}"`);
-		embed.setDescription(image.name)
 		embed.setFooter(sender.user.tag, sender.user.displayAvatarURL()).setTimestamp()
-		embed.setColor(`#${image.accentColor}`);
-		embed.setImage(image.contentUrl);
+		embed.setColor(Color.info);
+
+		// If no results
+		if(images.length === 0) {
+			embed.setColor(Color.error);
+			embed.setDescription("No images found.");
+			return await channel.send(embed);
+		}
+
+		// Filter based on NSFW channel needs
+		const imagesFiltered = images.filter(({ nsfw }) => nsfw === channel.nsfw);
+
+		// If no safe images are found
+		if(channel.nsfw === false && imagesFiltered.length === 0) {
+			embed.setColor(Color.error);
+			embed.setDescription(`No safe images found. \nTo make this an NSFW channel do \`${guildConfig.prefix}ch nsfw\``);
+			return await channel.send(embed);
+		}
+
+		// Pick a random image from search
+		const image = imagesFiltered[Math.floor(Math.random() * imagesFiltered.length)];
+
+		embed.setDescription(image.name)
+		embed.setImage(image.url);
 		return await channel.send(embed);
 
 	}
